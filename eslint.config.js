7b8fcbe31ade065de6bde45a1573b6b03ec0d186
Pranshu Patel <patelpranshu976@gmail.com>
2025-11-23 23:33:19 +0530

    init: initialize the project and eslint,husky setup

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintPluginImport from "eslint-plugin-import";
+
+export default defineConfig(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      import: eslintPluginImport,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "no-console": ["error", { allow: ["error"] }],
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+          pathGroups: [
+            {
+              pattern: "@/components/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+        },
+      ],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  }
+);
