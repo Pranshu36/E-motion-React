45bc6099e05a2c925c33745989ff664ef8778dc4
Pranshu Patel <patelpranshu976@gmail.com>
2025-12-10 22:46:49 +0530

    feat: create homepage gallery section UI

---
+++ src/features/homepage/constants/gallery.ts
@@ -0,0 +1,27 @@
+import image1 from '../../../assets/gallery/image1.png';
+import image10 from '../../../assets/gallery/image10.jpg';
+import image11 from '../../../assets/gallery/image11.jpg';
+import image12 from '../../../assets/gallery/image12.jpeg';
+import image2 from '../../../assets/gallery/image2.jpeg';
+import image3 from '../../../assets/gallery/image3.png';
+import image4 from '../../../assets/gallery/image4.webp';
+import image5 from '../../../assets/gallery/image5.webp';
+import image6 from '../../../assets/gallery/image6.avif';
+import image7 from '../../../assets/gallery/image7.jpg';
+import image8 from '../../../assets/gallery/image8.jpg';
+import image9 from '../../../assets/gallery/image9.jpg';
+
+export const GALLERY_IMAGES = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+];
